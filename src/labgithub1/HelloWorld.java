e657d69f0eca03d6ccd1bd2dddb1c864ce6c9ff6
Ido013 <tobal_no_1@hotmail.com>
2015-01-29 21:35:29 -0500

    test envoi 11

---
+++ src/labgithub1/HelloWorld.java
@@ -0,0 +1,11 @@
+package labgithub1;
+
+/**
+ *
+ * @author Weltall Alpha
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
